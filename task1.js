5bb66d6dad34bc6dbc79cf5d92be6c9b3d69fb8b
Dmitrii <belukhadmit@gmail.com>
2018-05-29 12:06:03 +0700

    fixed task1.js

--- task1.js
+++ task1.js
@@ -1,3 +1,11 @@
-const input = process.argv[2];
-let output = input.replace(/[РКН]/gi, '');
+
+const task1 = () => {
+    const input = process.argv[2];
+    let output = input.replace(/[РКН]/gi, '');
+
+    return output;
+}
+
+const output = task1();
+
 process.stdout.write(output);
